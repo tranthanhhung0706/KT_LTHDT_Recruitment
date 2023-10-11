799c2da1a471dff67806e013afa821c4db648340
tranthanhhung0706 <67146014+tranthanhhung0706@users.noreply.github.com>
2023-10-11 15:00:59 +0700

    DANH-NV Update code

--- src/main/java/com/tttn/ptithcm/constant/SessionConstant.java
+++
@@ -1,8 +0,0 @@
-package com.tttn.ptithcm.constant;
-
-public class SessionConstant {
-
-	public static final String SESSION_ADMIN_LOGIN_KEY = "admin";
-	public static final String SESSION_USER_LOGIN_KEY = "user";
-}
-
