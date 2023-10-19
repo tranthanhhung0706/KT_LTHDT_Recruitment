ca69aa66a7a6b8f094c4d50cbfc0231dac35ed36
danh290313 <danh290313@gmail.com>
2023-10-19 22:34:52 +0700

    asdfasf

--- Source code/src/main/resources/static/home/common/confirm_null.js
+++ Source code/src/main/resources/static/home/common/confirm_null.js
@@ -1,7 +1,9 @@
 
 function showErrorMsg(msg,field)
 {
-	$('#'+field).css('display','block');
+	if(msg != 'Độ dài không quá 50 kí tự'){
+		$('#'+field).css('display','block');
+	}
 	$("#errorMsg").html(msg);
 	$.colorbox({inline:true, href:$("#errorTip"),title:"Thông báo"});
 }
@@ -11,6 +13,11 @@ function checkForm(formId){
 	$("#"+formId).find(".require").each(function(i,e){
 		if($(e).val() == ''){
 			showErrorMsg($(e).attr('tips'),$(e).attr('attributes')); 
+			flag = false;
+			return false;
+		}
+		if($(e).val().length > 50){
+			showErrorMsg("Độ dài không quá 50 kí tự",$(e).attr('attributes')); 
 			flag = false;
 			return false;
 		}
