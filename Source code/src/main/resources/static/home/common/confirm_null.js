4e92dc3d4849b33f1b9f6b2941eeb8e3e929b9f7
danh290313 <danh290313@gmail.com>
2023-10-22 10:37:33 +0700

    sdafasfd

--- Source code/src/main/resources/static/home/common/confirm_null.js
+++ Source code/src/main/resources/static/home/common/confirm_null.js
@@ -1,9 +1,6 @@
 
 function showErrorMsg(msg,field)
 {
-	if(msg != 'Độ dài không quá 50 kí tự'){
-		$('#'+field).css('display','block');
-	}
 	$("#errorMsg").html(msg);
 	$.colorbox({inline:true, href:$("#errorTip"),title:"Thông báo"});
 }
@@ -13,11 +10,6 @@ function checkForm(formId){
 	$("#"+formId).find(".require").each(function(i,e){
 		if($(e).val() == ''){
 			showErrorMsg($(e).attr('tips'),$(e).attr('attributes')); 
-			flag = false;
-			return false;
-		}
-		if($(e).val().length > 50){
-			showErrorMsg("Độ dài không quá 50 kí tự",$(e).attr('attributes')); 
 			flag = false;
 			return false;
 		}
